55eaa11552ec0880a817c3d977f11e5b983d71c4
Brandon Brown <brandon@brandonb.ca>
2019-12-09 22:03:54 -0800

    Updates for dependency dependecies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,7 @@
+require('@babel/register');
+
 // Recursively require all tasks in ./gulp/tasks
 require('require-dir')('./gulp_tasks/tasks', {
-  recurse: true
-})
+  recurse: true,
+  noCache: true
+});
