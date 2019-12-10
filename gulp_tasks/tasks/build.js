87116521efaafd0b8e7f918f274ec860ac3a9bfc
Brandon Brown <brandon@brandonb.ca>
2019-12-10 11:38:20 -0800

    Remove leftover scraping code reference

--- gulp_tasks/tasks/build.js
+++ gulp_tasks/tasks/build.js
@@ -1,40 +1,21 @@
 import gulp from 'gulp';
-import { argv } from 'yargs';
 import runSequence from 'run-sequence';
 
 // Run the build
 gulp.task('build:dev', callback => {
-  if (argv.updateUnicode) {
-    runSequence(
-      // 'delete:dev',
-      'jekyll-build:dev',
-      ['styles:dev', 'scripts:dev'],
-      callback
-    );
-  } else {
-    runSequence(
-      // 'delete:dev',
-      'jekyll-build:dev',
-      ['styles:dev', 'scripts:dev'],
-      callback
-    );
-  }
+  runSequence(
+    // 'delete:dev',
+    'jekyll-build:dev',
+    ['styles:dev', 'scripts:dev'],
+    callback
+  );
 });
 
 gulp.task('build:prod', callback => {
-  if (argv.updateUnicode) {
-    runSequence(
-      // 'delete:prod',
-      'jekyll-build:prod',
-      ['styles:prod', 'scripts:prod'],
-      callback
-    );
-  } else {
-    runSequence(
-      // 'delete:prod',
-      'jekyll-build:prod',
-      ['styles:prod', 'scripts:prod'],
-      callback
-    );
-  }
+  runSequence(
+    // 'delete:prod',
+    'jekyll-build:prod',
+    ['styles:prod', 'scripts:prod'],
+    callback
+  );
 });
